108a4ac3fe4ffc9ad8c7efd7783d9f47cb2350e5
Dorofeev_Alexandr <amd_86@mail.ru>
2021-07-06 15:14:18 +0300

    Добавлено столкновение звезд с сохранением импулса

--- move_stars.py
+++ move_stars.py
@@ -1,10 +1,10 @@
-from random import randint
+from random import randint, uniform
 from math import pi
 from time import sleep
 import sys
 import pygame
 
-MAX_SPEED = 2
+MAX_SPEED = 0.5
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 
@@ -21,8 +21,8 @@ class Star:
         self.screen_height = screen_height
         self.x = randint(1, screen_width)
         self.y = randint(1, screen_height)
-        self.vx = randint(-MAX_SPEED, MAX_SPEED)
-        self.vy = randint(-MAX_SPEED, MAX_SPEED)
+        self.vx = uniform(-MAX_SPEED, MAX_SPEED)
+        self.vy = uniform(-MAX_SPEED, MAX_SPEED)
 
     def star_acceleration(self, others):
         acceleration_x = 0
@@ -52,9 +52,23 @@ class Star:
             self.vy = -abs(self.vy)
 
     def __add__(self, other):
+        impulse_x = self.vx * self.massa + other.vx * other.massa
+        impulse_y = self.vy * self.massa + other.vy * other.massa
         self.massa += other.massa
-        self.radius += other.radius
+        self.radius = int((self.massa / pi) ** 0.5)
+        self.vx = impulse_x / self.massa
+        self.vy = impulse_y / self.massa
         return self
+
+    def star_collisions(self, others):
+        for other in others:
+            if self != other:
+                if abs(self.x - other.x) < self.radius and abs(self.y - other.y) < self.radius:
+                    others.append(self + other)
+                    others.remove(self)
+                    others.remove(other)
+                    print(len(others))
+
 
     def update(self):
         self.color = (randint(0, 255),
@@ -74,7 +88,7 @@ class Star:
         return s
 
 
-stars = [Star(n, SCREEN_WIDTH, SCREEN_HEIGHT) for n in range(30)]
+stars = [Star(n, SCREEN_WIDTH, SCREEN_HEIGHT) for n in range(10)]
 
 
 def run_game():
@@ -84,13 +98,12 @@ def run_game():
     pygame.display.set_caption("Движение звезд")
     # Назначение цвета фона.
     bg_color = (0, 0, 0)
-    # При каждом проходе цикла перерисовывается экран.
-    screen.fill(bg_color)
+
     # Запуск основного цикла.
     while True:
         # Отслеживание событий клавиатуры и мыши.
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONDOWN:
                 sys.exit()
 
         # При каждом проходе цикла перерисовывается экран.
@@ -98,12 +111,13 @@ def run_game():
         for star in stars:
             # star.update()
             star.star_move(stars)
+            star.star_collisions(stars)
             star.star_paint(screen)
 
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
         # Pause.
-        sleep(0.05)
+        sleep(0.02)
 
 
 run_game()
